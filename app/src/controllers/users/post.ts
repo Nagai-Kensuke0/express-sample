662e77408d5b66c0ed1957fb406a14c62c21e2e2
Nagai-Kensuke0 <k.nagai430@gmail.com>
2022-11-20 19:03:49 +0900

    ユーザー登録API修正

--- app/src/controllers/users/post.ts
+++ app/src/controllers/users/post.ts
@@ -2,26 +2,26 @@ import { prisma } from "@/includes/prisma"
 import { Request, Response, NextFunction } from "express";
 import bcrypt from "bcrypt";
 
-export default async function (
+export default async (
   req: Request,
   res: Response,
   next: NextFunction
-): Promise<void> {
+): Promise<void> => {
+  try{
+    const { email, name, password } = req.body
+    const saltRounds = 10
+    const hashedPassword = bcrypt.hashSync(password, saltRounds)
 
-  const { email, name, password } = req.body;
+    await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword
+      }
+    })
 
-  const saltRounds = 10
-  const hashedPassword = bcrypt.hashSync(password, saltRounds);
-
-  await prisma.user.create({
-    data: {
-      email,
-      name,
-      password: hashedPassword
-    }
-  })  
-
-  res.status(200).json({
-    status: hashedPassword,
-  });
+    res.status(200).json()
+  } catch(error) {
+    next(error)
+  }
 }

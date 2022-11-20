7952769fe6e708d4ce1ce8507a95585b1a5bb68e
Nagai-Kensuke0 <k.nagai430@gmail.com>
2022-11-20 22:56:53 +0900

    ユーザー取得API作成

---
+++ app/src/controllers/users/get.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/includes/prisma"
+import { Request, Response, NextFunction } from "express";
+
+export default async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try{
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        name: true,
+      },
+    })
+
+    res.status(200).json(users)
+  } catch(error) {
+    next(error)
+  }
+}
--- app/src/routes/users.ts
+++ app/src/routes/users.ts
@@ -1,11 +1,14 @@
 import { Router, Request, Response, NextFunction } from "express";
 import UsersPost from "@/controllers/users/post"
+import UsersGet from "@/controllers/users/get"
+import UsersUserIdGet from "@/controllers/users/userId/get"
 import UsersUserIdDelete from "@/controllers/users/userId/delete"
 
-var express = require('express');
 var router = Router();
 
 router.post('/', UsersPost)
+router.get('/', UsersGet)
+router.get('/:userId', UsersUserIdGet)
 router.delete('/:userId', UsersUserIdDelete)
 
 module.exports = router;
